c4bc3ab2c0ec87ea604000d156769af9f523487e
Milind Goel <45682747+milindgoel15@users.noreply.github.com>
2021-10-24 13:19:12 +0530

    Band Name Generator

---
+++ BandName.py
@@ -0,0 +1,8 @@
+#Simple python program to generate a band name for yourself
+
+print("Welcome to band name generator! \n\n" )
+
+city_name = input("What city do you live in? \n")
+pet_name = input("What is the name of your pet? \n")
+
+print("The band name for your business shall be " + city_name + pet_name)
